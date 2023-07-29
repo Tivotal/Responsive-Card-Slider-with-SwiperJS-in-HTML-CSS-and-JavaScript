769eca75ab4b4cd0c918add68717aca31941576e
Tivotal <121335284+Tivotal@users.noreply.github.com>
2023-07-29 07:29:57 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,14 @@
+/* Created by Tivotal */
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
